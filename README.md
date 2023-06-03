b69b362eaff447a022b04665d6be586cb36fed10
Abhishek Anand <abhi966141shek@gmail.com>
2023-06-03 18:00:52 +0530

    seeder added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # brainerhub-api
+
+
+
+
+# Please follow the following steps:
+
+# api- 
+
+# step 1: clone the repository 'brainerhub-api'
+# step 2: Run 'npm install'
+# step 3: Run 'npx sequelize-cli db:migrate'
+# step 4: Run 'npx sequelize-cli db:seed:all' 
+# step 5: Run 'nodemon run start' or 'npm run start'
+
---
+++ seeders/20230603121545-demo-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('users', [
+    {
+      email: 'admin@gmail.com',
+      name: 'Admin',
+      password: '123456789',
+      createdAt: '2023-06-03 09:22:44',
+      updatedAt: '2023-06-03 09:22:44'
+    }
+  ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
